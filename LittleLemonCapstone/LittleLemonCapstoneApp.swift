446defcb25fd1ab7a50de163d8c9307fa909d043
rprice816 <125990290+rprice816@users.noreply.github.com>
2023-07-22 11:59:01 -0400

    Initial Commit

---
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by russell price on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
