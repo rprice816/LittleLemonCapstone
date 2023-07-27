8c5476e86bcc992d4b84c5437a4187297d1047bc
rprice816 <125990290+rprice816@users.noreply.github.com>
2023-07-27 11:13:29 -0400

    Capstone Project

--- LittleLemonCapstone/LittleLemonCapstoneApp.swift
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct LittleLemonCapstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Onboarding()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ LittleLemonCapstone/Views/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  LittleLemonCapstone
+//
+//  Created by russell price on 7/22/23.
+//
+
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
